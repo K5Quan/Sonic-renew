977e5926b202c9e956d51ae13a87e8faf13b578c
Bartosz Celmer <bartosz.celmer@i4-insight.com>
2026-08-19 22:33:03 +0200

    Improvements to the build process

--- compile-with-docker.sh
+++ compile-with-docker.sh
@@ -2,16 +2,21 @@
 clear
 set -euo pipefail
 
-IMAGE=uvk1-uvk5v3
+source load_settings.sh
 
 # Initialisation des variables par défaut
 CLEAN_BUILD=false
 EXTRA_ARGS=()
 PRESET=""
+FLASH=true
 
 # Boucle pour analyser TOUS les arguments
 while [[ $# -gt 0 ]]; do
   case "$1" in
+    --no-auto-flash)
+      FLASH=false
+      shift
+      ;;
     -c|--clean)
       CLEAN_BUILD=true
       shift
@@ -63,14 +68,20 @@ export MSYS_NO_PATHCONV=1
 build_preset() {
   local preset="$1"
   echo -e "\n 🚀 Building: ${preset}"
-  docker run --rm -u $(id -u):$(id -g) -v "$PWD":/src -w /src "$IMAGE" \
-  bash -c "cmake --preset ${preset} ${EXTRA_ARGS[@]+"${EXTRA_ARGS[@]}"} && \
-           cmake --build --preset ${preset} -j" \
+  docker run \
+    --rm \
+    -u $(id -u):$(id -g) \
+    -v "$PWD":/src \
+    -w /src \
+    -e VERSION_STRING_2=${VERSION_NO} \
+    "$IMAGE" \
+    bash -c "cmake --preset ${preset} -DTARGET=SONIC.${preset}.${VERSION_NO} -DVERSION_STRING_1=${VERSION_NO} -DVERSION_STRING_2=${VERSION_NO} ${EXTRA_ARGS[@]+"${EXTRA_ARGS[@]}"} && \
+             cmake --build --preset ${preset} -j" \
   2>&1 | sed "s|/src/|C:/Perso/Sonic/|g" \
        | sed -E '/^[[:space:]]+[A-Za-z0-9_]+(:[A-Za-z]+)?=/d; /--( Configuring|Generating) done/d; /-- Build files have been written to/d'
 
   docker run --rm -v "$PWD":/src -w /src "$IMAGE" \
-    arm-none-eabi-size ./build/${preset}/SONIC.${preset}.V50.elf
+    arm-none-eabi-size ./build/${preset}/SONIC.${preset}.${VERSION_NO}.elf
 
   echo "✅ Done: ${preset}"
 }
@@ -80,12 +91,12 @@ build_preset() {
 # ---------------------------------------------
 flash_preset() {
   local preset="$1"
-  local ifile="./build/${preset}/SONIC.${preset}.V50.bin"
+  local ifile="./build/${preset}/SONIC.${preset}.${VERSION_NO}.bin"
 
-  echo -e "\n⚡ Flashing ${preset} firmware on COM14..."
+  echo -e "\n⚡ Flashing ${preset} firmware on ${UPLOAD_PORT}..."
 
   if [[ -f "$ifile" ]]; then
-      python flash.py "$ifile" -p COM14
+      python flash.py "$ifile" -p ${UPLOAD_PORT}
       echo "✅ Flash ${preset} terminé avec succès !"
   else
       echo "❌ Erreur : Le fichier binaire est introuvable : $ifile"
@@ -105,8 +116,12 @@ if [[ "$PRESET" == "All" ]]; then
   echo "🎉 All presets built successfully!"
 
   # Si 'All' est compilé, on flashe uniquement le preset USB
-  flash_preset "USB"
+  if [ "$FLASH" = true ]; then
+    flash_preset "USB"
+  fi;
 else
   build_preset "$PRESET"
-  flash_preset "$PRESET"
+  if [ "$FLASH" = true ]; then
+    flash_preset "$PRESET"
+  fi;
 fi
